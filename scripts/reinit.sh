f22f5e97278b21e05aea7fd7bd4820bd660ce044
Tim.S <jshen44@fordham.edu>
2023-05-15 17:14:41 -0700

    script update

--- scripts/reinit.sh
+++ scripts/reinit.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# conda
-/notebooks/miniconda3/bin/conda init  && source ~/.bashrc
-
 # dot files
-wget https://go.momar.de/bashrc -O ~/.bashrc
+# wget https://go.momar.de/bashrc -O ~/.bashrc
+wget https://raw.githubusercontent.com/TimS-ml/My-ML/master/scripts/dots/.bashrc -O ~/.bashrc
 wget https://raw.githubusercontent.com/TimS-ml/My-ML/master/scripts/dots/.vimrc -O ~/.vimrc
 
+# conda
+# /notebooks/miniconda3/bin/conda init  && source ~/.bashrc
+
 # fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# ~/.fzf/install
 
