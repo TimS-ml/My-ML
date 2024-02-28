7960b3be1ba838abad322055d40911482edbfc0c
TimS <jshen44@fordham.edu>
2024-02-28 18:36:23 +0800

    quick commit

--- scripts/reinit.sh
+++ scripts/reinit.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 # dot files
-# wget https://go.momar.de/bashrc -O ~/.bashrc
-cp /notebooks/bashrc ~/.bashrc
-source ~/.bashrc
 # wget https://raw.githubusercontent.com/TimS-ml/My-ML/master/scripts/dots/.bashrc -O ~/.bashrc
 wget https://raw.githubusercontent.com/TimS-ml/My-ML/master/scripts/dots/.vimrc -O ~/.vimrc
-
-# conda
-# /notebooks/miniconda3/bin/conda init  && source ~/.bashrc
 
 # fzf
 # git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 # ~/.fzf/install
+
+eval "$(conda shell.bash hook)"
+conda activate /notebooks/env
+export PYTHONPATH="${PYTHONPATH}:/notebooks/env/"
+export PYTHONPATH="${PYTHONPATH}:/notebooks/env/utils/"
 
